c75d3511f547e6b8fa7e9ad85051a3c09782754f
xReMK <19bd1a0532.kmit@gmail.com>
2025-07-02 22:25:11 +0530

    Adding GameOfLife
    Adding GameOfLife

--- client-questai/src/App.jsx
+++ client-questai/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg' 
 import './App.css'
+import { GameOfLifeController } from './GameOfLife/GameOfLifeController';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* project starts */}
+    <div>
+      <GameOfLifeController columns={40} rows={40} />
+    </div>
     </>
   )
 }
